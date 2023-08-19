81bd8115c6501b010f1d5ccd90f143c201af5d9d
Dimitar <dimitartsvetkov89@gmail.com>
2023-08-19 04:12:08 +0300

    [us-1]-adding-v1-of-the-landing-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; // this is needed!
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app.routing";
+import { ComponentsModule } from "./components/components.module";
+import { PagesModule } from "./pages/pages.module";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-        ComponentsModule,
-        ExamplesModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, NavbarComponent],
+  imports: [
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    ComponentsModule,
+    PagesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NouisliderModule } from "ng2-nouislider";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { JwBootstrapSwitchNg2Module } from "jw-bootstrap-switch-ng2";
+
+import { LandingComponent } from "./landing/landing.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    NouisliderModule,
+    JwBootstrapSwitchNg2Module,
+  ],
+  declarations: [LandingComponent],
+})
+export class PagesModule {}
